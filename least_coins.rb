1dcb7e8d74c93be0d72c110b2507eb99a113866b
serendipidoussophia <scurranmoore@gmail.com>
2018-07-25 21:03:53 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -9,10 +9,10 @@ def least_coins(cents)
     cents=cents-quarters*25
   end
   
-  nickels = cents/25
-  if quarters==0
-    coins[;quarters]=0
-    elsif quarters != 0
-    coins [:quarters]=quarters
+  nickels = cents/5
+  if nickels==0
+    coins[;nickels]=0
+    elsif nickels != 0
+    coins [:nickels]=nickels
     cents=cents-quarters*25
   end
