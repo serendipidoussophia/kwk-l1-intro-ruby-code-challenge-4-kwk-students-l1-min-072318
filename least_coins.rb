b74b99ce50a45c94aa6094bfecdb5e5ebe615d12
serendipidoussophia <scurranmoore@gmail.com>
2018-07-25 21:09:00 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,5 +1,5 @@
 def least_coins(cents)
-  coins={quarters => 0, nickels => 0, dimes => 0, pennies => 0}
+  coins={;quarters => 0, nickels => 0, dimes => 0, pennies => 0}
   
   quarters = cents/25
   if quarters==0
